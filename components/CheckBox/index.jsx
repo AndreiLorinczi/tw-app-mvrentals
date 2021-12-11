228e087384e71af8bfd93a5630615aff14ba6cde
Darius <andrei.lorinczi00@e-uvt.ro>
2021-12-11 13:22:25 +0200

    html version

--- components/CheckBox/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./CheckBox.css";
-
-function CheckBox() {
-  return <div className="check-box"></div>;
-}
-
-export default CheckBox;
--- components/SearchIcon/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./SearchIcon.css";
-
-function SearchIcon() {
-  return <div className="search-icon"></div>;
-}
-
-export default SearchIcon;
